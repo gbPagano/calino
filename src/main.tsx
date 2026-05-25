6a6de19b53e6dd0c9a821a86b76f79e4b46faa9b
unknown <ivan@malinov.ski>
2026-05-25 14:30:52 +0200

    chore: disable service worker (not supported on GitHub Pages)

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {
-      // Service worker registration failed, app works without it
-    })
-  })
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
